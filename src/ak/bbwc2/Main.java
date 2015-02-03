b44517b6932a92f3d3ff4536a6ac4842da87d9aa
Alexander Korsos <alexander.w.korsos@gmail.com>
2015-02-03 12:36:57 -0600

    Update 1
    Ditched JSoup for something slightly more hands on. I’m going to use
    the built in parser in javax.swing. After we create ParseDelegator and
    pass the inputStream and parseCallback to it, we override the built in
    handleStartTag() function. That’s what I’m currently doing.

--- src/ak/bbwc2/Main.java
+++ src/ak/bbwc2/Main.java
@@ -1,8 +1,32 @@
 package ak.bbwc2;
 
-public class Main {
+import java.io.*;
+import java.net.URL;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.*;
+import javax.swing.text.html.parser.*;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args){
+        Reader r;
+
+        try   {
+            URL u = new URL("https://bluebox.com");
+            InputStream in = u.openStream();
+            r = new InputStreamReader(in);
+            ParserDelegator hp = new ParserDelegator();
+            System.out.println("After Parse");
+
+            hp.parse(r, new HTMLEditorKit.ParserCallback() {
+                @Override
+                public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+                    System.out.println(t);
+
+                }
+            }, true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
