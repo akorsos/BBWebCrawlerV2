2efee01d63da0a3ed02743e6ce83e11544f4abfa
Alexander Korsos <alexander.w.korsos@gmail.com>
2015-02-03 12:51:54 -0600

    Update 2
    Added comments and renamed some variables for readability, added an
    enum to hold found link elements

--- src/ak/bbwc2/Main.java
+++ src/ak/bbwc2/Main.java
@@ -2,26 +2,42 @@ package ak.bbwc2;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Enumeration;
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.*;
 import javax.swing.text.html.parser.*;
 
 public class Main {
     public static void main(String[] args){
-        Reader r;
+        Reader reader;
 
-        try   {
+        try{
             URL u = new URL("https://bluebox.com");
-            InputStream in = u.openStream();
-            r = new InputStreamReader(in);
-            ParserDelegator hp = new ParserDelegator();
-            System.out.println("After Parse");
 
-            hp.parse(r, new HTMLEditorKit.ParserCallback() {
+            //Create inputStream
+            InputStream input = u.openStream();
+
+            //Use stream to create streamReader
+            reader = new InputStreamReader(input);
+
+            //Create ParserDelegator
+            ParserDelegator pd = new ParserDelegator();
+
+            //Pass the streamReader and the ParserCallback class
+            pd.parse(reader, new HTMLEditorKit.ParserCallback() {
+
+                //Override the handleStartTag function in ParserCallback
                 @Override
                 public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
-                    System.out.println(t);
 
+                    //An A tag defines a hyperlink
+                    if(t == HTML.Tag.A)  {
+
+                        //Create an enum to house elements
+                        Enumeration attrNames = a.getAttributeNames();
+
+
+                    }
                 }
             }, true);
 
